c6523f2494020530b4f85c21c732567453446dc0
gimeast <dudu.gimeast@gmail.com>
2024-06-25 23:50:01 +0900

    프로젝트 생성 / 의존성 추가

---
+++ springsecurity-v6/src/main/java/io/security/springsecurityv6/SpringsecurityV6Application.java
@@ -0,0 +1,13 @@
+package io.security.springsecurityv6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityV6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecurityV6Application.class, args);
+    }
+
+}
---
+++ springsecurity-v6/src/test/java/io/security/springsecurityv6/SpringsecurityV6ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.security.springsecurityv6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityV6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
