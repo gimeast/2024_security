4d740891f8175e2502d1769c47a846910a7729f8
gimeast <dudu.gimeast@gmail.com>
2024-07-24 23:10:17 +0900

    인증 프로세스
    - 익명 인증 사용자 – anonymous()

--- springsecurity-v6/src/main/java/io/security/springsecurityv6/IndexController.java
+++ springsecurity-v6/src/main/java/io/security/springsecurityv6/IndexController.java
@@ -1,5 +1,9 @@
 package io.security.springsecurityv6;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +34,25 @@ public class IndexController {
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+    
+    @GetMapping("/anonymous")
+    public String anonymous() {
+        return "anonymous";
+    }
+
+    @GetMapping("/authentication")
+    public String authentication(Authentication authentication) {
+        if(authentication instanceof AnonymousAuthenticationToken) {
+            return "anonymous";
+        } else {
+            return "not anonymous";
+        }
+    }
+
+    @GetMapping("/anonymous-context")
+    public String anonymousContext(@CurrentSecurityContext SecurityContext securityContext) {
+        return securityContext.getAuthentication().getName();
     }
 
 }
