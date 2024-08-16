52da3cb7526555b445e177013fe93f9463c6f8c4
gimeast <dudu.gimeast@gmail.com>
2024-08-16 20:46:58 +0900

    SecurityContextRepository / SecurityContextHolderFilter

--- springsecurity-v6/src/main/java/io/security/springsecurityv6/config/SecurityConfig.java
+++ springsecurity-v6/src/main/java/io/security/springsecurityv6/config/SecurityConfig.java
@@ -1,20 +1,18 @@
 package io.security.springsecurityv6.config;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-
-import java.io.IOException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -22,45 +20,32 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        AuthenticationManager authenticationManager = builder.build();
         http
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated()) //어떤 요청이든 인증하겠다는 의미
-//              .formLogin(Customizer.withDefaults()); //기본적으로 주어지는 폼로그인을 사용하겠다는 의미
-                .formLogin(form -> form
-//                        .loginPage("/loginPage")
-                        .loginProcessingUrl("/loginProc")
-                        .defaultSuccessUrl("/", false) //true=인증에 성공하면 무조건 이곳으로 이동한다. false=인증이 필요한 곳을 방문 하였으면 인증을 완료 후 방문하려는 위치로 리다이렉트된다.
-                        .failureUrl("/failed")
-                        .usernameParameter("userId")
-                        .passwordParameter("passwd")
-                        .successHandler(new AuthenticationSuccessHandler() { //failureUrl을 무시하고 해당 핸들러를 우선시한다.
-                            @Override
-                            public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                                System.out.println("authentication success : " + authentication);
-                                response.sendRedirect("/home");
-                            }
-                        })
-                        .failureHandler(new AuthenticationFailureHandler() { //defaultSuccessUrl을 무시하고 해당 핸들러를 우선시한다.
-                            @Override
-                            public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-                                System.out.println("exception: " + exception.getMessage());
-                                response.sendRedirect("/login");
-                            }
-                        })
-                        .permitAll()
-                );
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/login").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+//                .securityContext(securityContext ->securityContext.requireExplicitSave(false))
+                .authenticationManager(authenticationManager)
+                .addFilterBefore(customAuthenticationFilter(http, authenticationManager), UsernamePasswordAuthenticationFilter.class)
+        ;
 
         return http.build();
     }
 
+     public CustomAuthenticationFilter customAuthenticationFilter(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
+         CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(http);
+         customAuthenticationFilter.setAuthenticationManager(authenticationManager);
+         return customAuthenticationFilter;
+     }
+
     @Bean
     public UserDetailsService userDetailsService() {
-        return new CustomUserDetailsService();
+        //yml에서 설정한 정보와 클래스에 설정한 정보가 중복된다면 클래스에 설정한 정보가 우선 적용 된다.
+        UserDetails user = User.withUsername("user").password("{noop}1111").roles("USER").build();
+        return new InMemoryUserDetailsManager(user);
     }
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        //yml에서 설정한 정보와 클래스에 설정한 정보가 중복된다면 클래스에 설정한 정보가 우선 적용 된다.
-//        UserDetails user = User.withUsername("user").password("{noop}1111").roles("USER").build();
-//        return new InMemoryUserDetailsManager(user);
-//    }
 
 }
