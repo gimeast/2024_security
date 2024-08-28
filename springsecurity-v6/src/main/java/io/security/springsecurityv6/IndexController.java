a5ed556cfcf5a42e9ca764d61ba99762ef831792
gimeast <dudu.gimeast@gmail.com>
2024-08-28 23:00:33 +0900

    예외 처리 - exceptionHandling()

--- springsecurity-v6/src/main/java/io/security/springsecurityv6/IndexController.java
+++ springsecurity-v6/src/main/java/io/security/springsecurityv6/IndexController.java
@@ -31,5 +31,9 @@ public class IndexController {
     public String login() {
         return "login";
     }
+    @GetMapping("/denied")
+    public String denied() {
+        return "denied";
+    }
 
 }
--- springsecurity-v6/src/main/java/io/security/springsecurityv6/SecurityConfig.java
+++ springsecurity-v6/src/main/java/io/security/springsecurityv6/SecurityConfig.java
@@ -1,15 +1,24 @@
 package io.security.springsecurityv6;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.io.IOException;
 
 
 @EnableWebSecurity
@@ -21,8 +30,20 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/login").permitAll()
-                        .anyRequest().authenticated())
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
                 .formLogin(Customizer.withDefaults())
+                .exceptionHandling(exeption -> exeption
+//                        .authenticationEntryPoint((request, response, authException) -> {
+//                            System.out.println("exception: " + authException.getMessage());
+//                            response.sendRedirect("/login");
+//                        })
+                        .accessDeniedHandler((request, response, accessDeniedException) -> {
+                            System.out.println("exception: " + accessDeniedException.getMessage());
+                            response.sendRedirect("/denied");
+                        })
+                );
         ;
 
         return http.build();
